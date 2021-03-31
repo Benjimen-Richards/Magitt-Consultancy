d465661c0019917006d4eb8840722b3cd7340afa
alfred-richards-au9 <67587493+alfred-richards-au9@users.noreply.github.com>
2021-03-31 16:47:13 +0530

    .

---
+++ frontend/src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+
+export default function RadioButtonsGroup() {
+  const [value, setValue] = React.useState("female");
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <FormControl component="fieldset">
+      <FormLabel component="legend">Gender</FormLabel>
+      <RadioGroup
+        aria-label="gender"
+        name="gender1"
+        value={value}
+        onChange={handleChange}
+      >
+        <FormControlLabel
+          value="female"
+          control={<Radio />}
+          label="Female"
+          style={{
+            display: "",
+            width: "10px",
+            border: "1px solid black",
+            height: "2px",
+          }}
+        />
+
+        <FormControlLabel value="other" control={<Radio />} label="Other" />
+        <FormControlLabel
+          value="disabled"
+          disabled
+          control={<Radio />}
+          label="(Disabled option)"
+        />
+      </RadioGroup>
+    </FormControl>
+  );
+}
